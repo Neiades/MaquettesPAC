99377ed602c372803af92e94058ff6b8f1c41210
Julien Dupison <Neiades1@gmail.com>
2016-05-27 19:16:39 +0200

    je sais plus ce que j'ai fait mais ça doit être mege important

--- podcast.php
+++ podcast.php
@@ -137,31 +137,31 @@
 
                             <div class="col-md-6">
 
-                              <div id="podcast" class="jp-jplayer"></div>
-                  						<div id="jp_container_1" class="jp-audio" style="width:250px;" role="application" aria-label="media player">
-                  							<div class="jp-type-single">
-                  								<div class="jp-gui jp-interface">
-                  									<div class="jp-controls">
-                  										<button class="jp-play" role="button" tabindex="0">Jouer</button>
-                  										<button class="jp-stop" role="button" tabindex="0">Stop</button>
-                  									</div>
-                  									<div class="jp-volume-controls" style="margin-left : -180px;">
-                  										<button class="jp-mute" role="button" tabindex="0">mute</button>
-                  										<button class="jp-volume-max" role="button" tabindex="0">max volume</button>
-                  										<div class="jp-volume-bar">
-                  											<div class="jp-volume-bar-value"></div>
-                  										</div>
-                  									</div>
-                  								</div>
-                  								<div class="jp-details">
-                  									<div class="jp-title" aria-label="title" id="titre"></div>
-                  								</div>
-                  								<div class="jp-no-solution">
-                  									<span>Update Required</span>
-                  									To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-                  								</div>
-                  							</div>
-                  						</div>
+                                <div id="podcast" class="jp-jplayer"></div>
+	      						<div id="jp_container_1" class="jp-audio" style="width:250px;" role="application" aria-label="media player">
+	      							<div class="jp-type-single">
+	      								<div class="jp-gui jp-interface">
+	      									<div class="jp-controls">
+	      										<button style="transition:0s;" class="jp-play" role="button" tabindex="0">Jouer</button>
+	      										<button class="jp-stop" role="button" tabindex="0">Stop</button>
+	      									</div>
+	      									<div class="jp-volume-controls" style="margin-left : -180px;">
+	      										<button class="jp-mute" role="button" tabindex="0">mute</button>
+	      										<button class="jp-volume-max" role="button" tabindex="0">max volume</button>
+	      										<div class="jp-volume-bar">
+	      											<div class="jp-volume-bar-value"></div>
+	      										</div>
+	      									</div>
+	      								</div>
+	      								<div class="jp-details">
+	      									<div class="jp-title" aria-label="title" id="titre"></div>
+	      								</div>
+	      								<div class="jp-no-solution">
+	      									<span>Update Required</span>
+	      									To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+	      								</div>
+	      							</div>
+	      						</div>
 
 
                             </div>
@@ -325,7 +325,7 @@
 						ready: function (event) {
 							$(this).jPlayer("setMedia", {
 								mp3: path_podcast,
-                title: title_podcast,
+                				title: title_podcast,
 							});
 						},
 						swfPath: "js",
